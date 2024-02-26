8df6afa90611b499b80c63ad326a25da22abe507
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-02-26 21:14:03 +0000

    helper methods 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,12 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new"
   end
 
   def index
     matching_movies = Movie.all
 
     @list_of_movies = matching_movies.order({ :created_at => :desc })
-
-    respond_to do |format|
-      format.json do
-        render json: @list_of_movies
-      end
-
-      format.html do
-        render({ :template => "movies/index" })
-      end
-    end
   end
 
   def show
@@ -27,8 +15,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render({ :template => "movies/show" })
   end
 
   def create
@@ -38,9 +24,9 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render "movies/new"
     end
   end
 
@@ -50,8 +36,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render({ :template => "movies/edit" })
   end
 
   def update
@@ -63,9 +47,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movies_url, :notice => "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movies_url, :alert => "Movie failed to update successfully."
     end
   end
 
@@ -75,6 +59,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, :notice => "Movie deleted successfully."
   end
 end
